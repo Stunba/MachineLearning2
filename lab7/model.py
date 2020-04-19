9da48e56baeeb6f9cd5f82018148d034c1899220
Artoim Bastun <a.bastun@verv.com>
2020-04-20 00:35:42 +0300

    update

--- lab7/model.py
+++ lab7/model.py
@@ -9,12 +9,7 @@ class RNN(nn.Module):
         
         self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx = pad_idx)
         
-        self.cnn = nn.Sequential(
-            nn.Conv1d(in_channels=embedding_dim, out_channels=32, kernel_size=3),
-            nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2, stride=2)
-        )
-        self.rnn = nn.LSTM(32, 
+        self.rnn = nn.LSTM(embedding_dim, 
                            hidden_dim, 
                            num_layers=n_layers, 
                            bidirectional=bidirectional, 
@@ -29,7 +24,6 @@ class RNN(nn.Module):
         #text = [sent len, batch size]
         
         embedded = self.dropout(self.embedding(text))
-        embedded = self.cnn(embedded)
         
         #embedded = [sent len, batch size, emb dim]
         
@@ -55,3 +49,54 @@ class RNN(nn.Module):
         #hidden = [batch size, hid dim * num directions]
             
         return self.fc(hidden)
+    
+class RNN2(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim, pad_idx, n_layers=2, 
+                 bidirectional=True, dropout=0.5):
+        
+        super().__init__()
+        
+        self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx = pad_idx)
+        
+        self.rnn = nn.LSTM(embedding_dim, 
+                           hidden_dim, 
+                           num_layers=n_layers, 
+                           bidirectional=bidirectional, 
+                           dropout=dropout)
+        
+        self.fc1 = nn.Linear(hidden_dim * 2, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+        
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, text, text_lengths):
+        
+        #text = [sent len, batch size]
+        
+        embedded = self.dropout(self.embedding(text))
+        
+        #embedded = [sent len, batch size, emb dim]
+        
+        #pack sequence
+        packed_embedded = nn.utils.rnn.pack_padded_sequence(embedded, text_lengths, enforce_sorted=False)
+        
+        packed_output, (hidden, cell) = self.rnn(packed_embedded)
+        
+        #unpack sequence
+        output, output_lengths = nn.utils.rnn.pad_packed_sequence(packed_output)
+
+        #output = [sent len, batch size, hid dim * num directions]
+        #output over padding tokens are zero tensors
+        
+        #hidden = [num layers * num directions, batch size, hid dim]
+        #cell = [num layers * num directions, batch size, hid dim]
+        
+        #concat the final forward (hidden[-2,:,:]) and backward (hidden[-1,:,:]) hidden layers
+        #and apply dropout
+        
+        hidden = self.dropout(torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim = 1))
+                
+        #hidden = [batch size, hid dim * num directions]
+        
+        hidden = self.fc1(hidden)
+        return self.fc2(hidden)
